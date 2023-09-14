9fead31b028308b86d97847d94632ab3ac0f4c15
iamWGO <gottlieb.waleerat@gmail.com>
2023-09-14 11:30:32 +0200

    Exception - example

---
+++ src/main/java/com/wgoweb/exception/Main.java
@@ -0,0 +1,28 @@
+package com.wgoweb.exception;
+
+import java.io.IOException;
+
+/*
+Why we use exception
+*  - for example : read file, api, exception rule  ( ex. device)
+*  - for indicating error or other exceptional circumstances
+*  - For information specifically relate to the error or exceptional circumstance
+*  - don't use for standard app control flow or conditional logic
+*  - don't use for bypassing parameters or method return value
+* */
+public class Main {
+  public static void main(String[] args) {
+    int i = 12;
+    int j = 2;
+
+    try {
+      int result = i / (j - 2);
+      System.out.println(result);
+
+    } catch (ArithmeticException ex) {
+      System.out.println("Invalid math operation - " + ex.getMessage());
+    } catch (Exception ex) {
+      System.out.println("Error: " + ex.getMessage());
+    }
+  }
+}
